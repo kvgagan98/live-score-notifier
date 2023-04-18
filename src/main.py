f45663d3492b68a7904f83cb0bbe787adfb76f26
kvgagan98 <kvgagan98@gmail.com>
2023-04-17 22:04:11 -0700

    Map game date to matchups

--- src/main.py
+++ src/main.py
@@ -4,5 +4,6 @@ if __name__ == "__main__":
     url = "https://www.espncricinfo.com/series/indian-premier-league-2023-1345038/match-schedule-fixtures-and-results" 
     schedule = scrape.scrapeSchedule(url)
     with open('main.txt','w') as file:
+        print(len(schedule))
         file.write(str(schedule))
     print("DONE WRITING")
--- src/scrape.py
+++ src/scrape.py
@@ -5,12 +5,51 @@ def scrapeSchedule(url):
     page = requests.get(url)
     print("Type = ", type(page.content))
     soup = BeautifulSoup(page.content, 'html.parser')
-    schedule = soup.find_all(attrs= {
-                                     'class': 'ds-p-0',   
+    panelDiv = soup.find_all(attrs= {
+                                     'class': 'ds-p-4'   
                                      })
-    dates = soup.find_all(attrs= {
-                                  'class': 'ds-text-compact-xs ds-font-bold ds-w-24' 
-                                  })
-    for date in dates:
-        print(date.get_text())
-    return (schedule)
+    numSpaces = 20
+    print("Date", numSpaces*" ", "Match Details", numSpaces*" ", "Time")
+    matchDetailList = []
+    gameDateList = []
+    schedule = {}
+    for div in panelDiv:
+        print(div.get_text())
+        print("   ")
+        gameDates = div.find_all('div', attrs = {'class': 'ds-text-compact-xs ds-font-bold ds-w-24'})
+        matchDetails = div.find_all('div', attrs = {'class': 'ds-flex ds-flex-col ds-mt-2 ds-mb-2'})
+
+        for gameDate in gameDates:
+            if len(gameDate) == 0:
+                try:
+                    currGameDate = prevGameDate
+                except Exception as e:
+                    raise e
+                print("Length = 0")
+            else:
+                currGameDate = gameDate.get_text()
+                prevGameDate = currGameDate
+                print("Length != 0")
+            gameDateList.append(currGameDate)
+            print("gameDate = ", currGameDate)
+
+        for matchDetail in matchDetails:
+            print("matchDetail length = ", len(matchDetail))
+            print(matchDetail.get_text())
+            matchDetailList.append(matchDetail.get_text())
+
+    print("GameDateList = ", gameDateList)
+    print("GameDateList Len = ", len(gameDateList))
+    print(" ")
+    print("Match Detail List = ", matchDetailList)
+    print("Match Detail List Len = ", len(matchDetailList))
+
+    for i in range(len(gameDateList)):
+        #print(gameDateList.pop(), "--->", matchDetailList.pop())
+        schedule[matchDetailList.pop()] = gameDateList.pop()
+
+    for key,value in schedule.items():
+        print(key, "--->", value)
+    #print("Schedule = ", schedule)
+
+    return panelDiv
